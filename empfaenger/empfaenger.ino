45b84ba10eb0d196262abc162d881ad0b6b58d55
Max Kobrow <max.kobrow@gmail.com>
2021-12-14 12:54:52 +0100

    removed clock functionality

--- empfaenger/empfaenger.ino
+++ empfaenger/empfaenger.ino
@@ -1,9 +1,7 @@
 byte dataPins[] = {53,51,49,47,45,43,41,39};
-byte clockPin = 23;
 
 char data[32];
 byte dataCounter = 0;
-byte lastClock = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,7 +9,6 @@ void setup() {
   for(int i = 0; i < 8; i++) {
     pinMode(dataPins[i], INPUT_PULLUP);
   }
-  pinMode(clockPin, INPUT_PULLUP);
 
   Serial.begin(9600); 
 
@@ -19,22 +16,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  byte myClock = digitalRead(clockPin);
-  if(myClock != lastClock){
-    lastClock = myClock;
-    char character;
-
+  char character;
+  for(int i = 0; i < 8; i++){
+    bitWrite(character, i, digitalRead(dataPins[7-i]));
+  }
+  if(character == 2){
     for(int i = 0; i < 8; i++){
-      bitWrite(character, i, digitalRead(dataPins[7-i]));
+        bitWrite(character, i, digitalRead(dataPins[7-i]));
     }
-
-    Serial.println(dataCounter);
-    if(dataCounter == 31){
-      dataCounter = 0;
-    }
-    data[dataCounter] = character;
-    dataCounter++;
+    do {
+      delay(1000);
+      data[dataCounter] = character;
+      dataCounter++;
+      Serial.print(character);
+      for(int i = 0; i < 8; i++){
+        bitWrite(character, i, digitalRead(dataPins[7-i]));
+      }
+    } while(character != 3);
     Serial.println(data);
-    delay(1000);
   }
 }
--- sender/sender.ino
+++ sender/sender.ino
@@ -1,7 +1,5 @@
-int clockPin = 16;
 int dataPins[] = {15,13,2,0,4,5,12,14};
 
-int myClock = 0;
 char message[] = "Hello, World";
 
 void setup() {
@@ -10,28 +8,34 @@ void setup() {
   for(int i = 0; i < 8; i++) {
     pinMode(dataPins[i],OUTPUT);
   }
-
-  pinMode(clockPin,OUTPUT);
-
+  
   Serial.begin(9600);
 
+  // write STX Start of Text
+  for(int i = 0; i < 8; i++) {
+    digitalWrite(dataPins[i], bitRead(2, i));
+  }
+
+  delay(1000);
+  
   for(int i = 0; i < sizeof(message); i++) {
     Serial.print(message[i]);
     for(int j = 7; j >= 0; j--) {
       digitalWrite(dataPins[j], bitRead(message[i],j));
       Serial.print(bitRead(message[i],j));
     }
-
-    myClock = !myClock;
-    digitalWrite(clockPin, myClock);
-    Serial.print(" ");
-    Serial.println(myClock);
+    
     delay(1000);
   }
+
+  // write ETX End of Text
   for(int i = 0; i < 8; i++) {
-    digitalWrite(dataPins[i],LOW);
+    digitalWrite(dataPins[i], bitRead(3, i));
   }
-  digitalWrite(clockPin, LOW);
+  delay(1000);
+  for(int i = 0; i < 8; i++) {
+    digitalWrite(dataPins[i], LOW);
+  }
 }
 
 void loop() {

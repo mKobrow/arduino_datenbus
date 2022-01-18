bc15f71b8f2d08ecaf875e26acb042bf1dd52cea
Max <max.kobrow@gmail.com>
2022-01-18 13:42:37 +0100

    added lcd support and comments

--- empfaenger/empfaenger.ino
+++ empfaenger/empfaenger.ino
@@ -1,38 +1,67 @@
+// Einbinden der nötigen Bibliotheken für LCD Display und Initialiserung
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// Festlegen der Eingangspins (most significant to least significant)
 byte dataPins[] = {53,51,49,47,45,43,41,39};
 
-char data[32];
 byte dataCounter = 0;
 
 void setup() {
+  // Initialisierung des Displays
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Waiting...");
+
+  // Einstellen des Pullup Wiederstandes fuer die Eingangspins
   for(int i = 0; i < 8; i++) {
     pinMode(dataPins[i], INPUT_PULLUP);
   }
-
-  Serial.begin(9600); 
-
 }
 
-void loop() {
+void loop() {  
+  // lese alle Sekunde ein Zeichen von den Pins
   char character;
-  for(int i = 0; i < 8; i++){
-    bitWrite(character, i, digitalRead(dataPins[7-i]));
-  }
+  character = readPins();
+  delay(1000);
+
+  // wenn Zeichen STX (start of text)
   if(character == 2){
-    for(int i = 0; i < 8; i++){
-        bitWrite(character, i, digitalRead(dataPins[7-i]));
-    }
+    // leere das Display und lese das nächste Zeichen
+    character = readPins();
+    lcd.clear();
     do {
-      delay(1000);
-      data[dataCounter] = character;
-      dataCounter++;
-      Serial.print(character);
-      for(int i = 0; i < 8; i++){
-        bitWrite(character, i, digitalRead(dataPins[7-i]));
+      // schreibe das Zeichen auf das Display, isPrintable faengt Fehler ab
+      if(isPrintable(character)) {
+        print_lcd(lcd, character, dataCounter);
       }
+      // warte, lese naechstes Zeichen
+      delay(1000);
+      dataCounter++;
+      character = readPins();
     } while(character != 3);
-    Serial.println(data);
+    dataCounter = 0;
   }
+}
+
+// Funktion zum Lesen eines char von den Eingangspins
+char readPins () {
+  char c;
+  for(int i = 0; i < 8; i++){
+    bitWrite(c, i, digitalRead(dataPins[7-i]));
+  }
+  return c;
+}
+
+// schreibt auf LCD und beachtet die maximale LCD laenge
+void print_lcd (LiquidCrystal_I2C lcd, char c, byte counter) {
+  if (counter < 16) {
+    lcd.setCursor(counter, 0);
+  } else {
+    lcd.setCursor(counter-16, 1);
+  }
+
+  lcd.print(c);
 }

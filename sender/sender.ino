c41579d23e253f436c71190629c5c87ed885016c
Max <max.kobrow@gmail.com>
2022-01-18 14:04:40 +0100

    comments in sender

--- sender/sender.ino
+++ sender/sender.ino
@@ -1,45 +1,43 @@
+// Ausgangspins (most significant to least significant)
 int dataPins[] = {15,13,2,0,4,5,12,14};
 
-char message[] = "Hello, World";
+char message[] = "Hello, World! This may be too long";
 
 void setup() {
-  // put your setup code here, to run once:
+  // delay für Entprellung des Reset Tasters
   delay(1000);
+  // Ausgangspins initialisieren
   for(int i = 0; i < 8; i++) {
     pinMode(dataPins[i],OUTPUT);
   }
-  
-  Serial.begin(9600);
 
-  // write STX Start of Text
+  // schreibe STX (start of text)
   for(int i = 0; i < 8; i++) {
     digitalWrite(dataPins[i], bitRead(2, i));
   }
 
   delay(1000);
   
+  // Schleife über Buchstaben der Nachricht
   for(int i = 0; i < sizeof(message); i++) {
-    Serial.print(message[i]);
+    // Schleife über Bits der Buchstaben
     for(int j = 7; j >= 0; j--) {
       digitalWrite(dataPins[j], bitRead(message[i],j));
-      Serial.print(bitRead(message[i],j));
     }
-    
     delay(1000);
   }
 
-  // write ETX End of Text
+  // schreibe ETX (end of text)
   for(int i = 0; i < 8; i++) {
     digitalWrite(dataPins[i], bitRead(3, i));
   }
   delay(1000);
+  // alle Pins low damit die LEDs nicht dauerhaft leuchten
   for(int i = 0; i < 8; i++) {
     digitalWrite(dataPins[i], LOW);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  
+  // wird nicht benoetigt
 }

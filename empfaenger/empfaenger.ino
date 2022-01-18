5a4bec5c39fb8b17d4ded1a25f6279393ebfda5e
Max <max.kobrow@gmail.com>
2022-01-18 10:32:02 +0100

    refactoring

--- empfaenger/empfaenger.ino
+++ empfaenger/empfaenger.ino
@@ -1,11 +1,12 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 byte dataPins[] = {53,51,49,47,45,43,41,39};
 
 char data[32];
 byte dataCounter = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-
   for(int i = 0; i < 8; i++) {
     pinMode(dataPins[i], INPUT_PULLUP);
   }
@@ -15,7 +16,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   char character;
   for(int i = 0; i < 8; i++){
     bitWrite(character, i, digitalRead(dataPins[7-i]));
